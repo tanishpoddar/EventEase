6b879b17064f9f53187db5085dba03a4c87f6c5c
Tanish Poddar <72662261+tanishpoddar@users.noreply.github.com>
2025-03-07 23:39:17 +0530

    added and deleted files

---
+++ migrations/20250306001300-add-password-to-users.js
@@ -0,0 +1,12 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.addColumn("Users", "password", {
+      type: Sequelize.STRING,
+      allowNull: false,
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn("Users", "password");
+  },
+};
